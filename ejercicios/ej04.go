1fd997363913684e202c890f6f9582936c14d767
lucasmz7 <lucasmierez007@gmail.com>
2025-06-02 19:54:34 -0300

    Ejercicio 4 y 5) resuelto

--- ejercicios/ej04.go
+++ ejercicios/ej04.go
@@ -1,6 +1,7 @@
 package ejercicios
 
 import (
+	"fmt"
 	"untref-ayp2/guia-abb/binarytree"
 
 	"golang.org/x/exp/constraints"
@@ -11,34 +12,59 @@ type TreeSet[T constraints.Ordered] struct {
 }
 
 func NewTreeSet[T constraints.Ordered](elements ...T) *TreeSet[T] {
-	// Implementar
-	return nil
+	ts := &TreeSet[T]{set: binarytree.NewBinarySearchTree[T]()}
+	for _, n := range elements {
+		ts.Add(n)
+	}
+	return ts
 }
 
 func (ts *TreeSet[T]) Add(elements ...T) {
-	// Implementar
+	for _, n := range elements {
+		if !ts.set.Search(n) {
+			ts.set.Insert(n)
+		}
+	}
 }
 
 func (ts *TreeSet[T]) Size() int {
-	// Implementar
-	return 0
+	return ts.set.Size()
 }
 
 func (ts *TreeSet[T]) Contains(element T) bool {
-	// Implementar
-	return false
+	return ts.set.Search(element)
 }
 
 func (ts *TreeSet[T]) Remove(element T) {
-	// Implementar
+	ts.set.Remove(element)
 }
 
 func (ts *TreeSet[T]) Values() []T {
-	// Implementar
-	return nil
+	var arr []T
+	return ts.valueRecorrido(arr, ts.set.GetRoot())
+}
+
+func (ts *TreeSet[T]) valueRecorrido(arr []T, n *binarytree.BinaryNode[T]) []T {
+	if n == nil {
+		return arr
+	}
+
+	arr = ts.valueRecorrido(arr, n.GetLeft())
+	arr = append(arr, n.GetData())
+	arr = ts.valueRecorrido(arr, n.GetRight())
+
+	return arr
 }
 
 func (ts *TreeSet[T]) String() string {
-	// Implementar
-	return ""
+	values := ts.Values()
+	s := "Set: {"
+	if ts.set.Size() != 0 {
+		for _, n := range values {
+			s += fmt.Sprintf("%v ", n)
+		}
+		s = s[0 : len(s)-1]
+	}
+	s += "}"
+	return s
 }
