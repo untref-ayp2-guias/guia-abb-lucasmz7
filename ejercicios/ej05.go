07acf7be8f94c3eaebac2cb426dd96637d396288
lucasmz7 <lucasmierez007@gmail.com>
2025-06-04 22:38:19 -0300

    Ejercicio 6) resuelto

--- ejercicios/ej05.go
+++ ejercicios/ej05.go
@@ -1,6 +1,7 @@
 package ejercicios
 
 import (
+	"errors"
 	"untref-ayp2/guia-abb/binarytree"
 	"untref-ayp2/guia-abb/stack"
 
@@ -20,19 +21,39 @@ type BSTInOrderIterator[T constraints.Ordered] struct {
 // Retorna:
 //   - un Iterator.
 func NewBSTInOrderIterator[T constraints.Ordered](bst *binarytree.BinarySearchTree[T]) Iterator[T] {
-	// Implementar
-	return nil
+	it := &BSTInOrderIterator[T]{
+		internalStack: stack.NewStack[*binarytree.BinaryNode[T]](),
+	}
+	recorridoInOrder(it.internalStack, bst.GetRoot())
+
+	return it
+}
+
+func recorridoInOrder[T constraints.Ordered](stack *stack.Stack[*binarytree.BinaryNode[T]], root *binarytree.BinaryNode[T]) *stack.Stack[*binarytree.BinaryNode[T]] {
+	if root == nil {
+		return stack
+	}
+
+	stack = recorridoInOrder(stack, root.GetRight())
+	stack.Push(root)
+	stack = recorridoInOrder(stack, root.GetLeft())
+
+	return stack
 }
 
 // HasNext indica si hay un siguiente dato.
 func (it *BSTInOrderIterator[T]) HasNext() bool {
-	// Implementar
-	return false
+	return !it.internalStack.IsEmpty()
 }
 
 // Next devuelve el siguiente dato, respetando el recorrido InOrder.
 func (it *BSTInOrderIterator[T]) Next() (T, error) {
-	// Implementar
-	var zero T
-	return zero, nil
+	if !it.HasNext() {
+		var zero T
+		err := errors.New("árbol vacío")
+		return zero, err
+	}
+
+	node, _ := it.internalStack.Pop()
+	return node.GetData(), nil
 }
